5fb2104eec1c7684a91c402ceabdbaa22f42f4fb
Monu Sharma <monupsr@yahoo.com>
2022-02-14 08:30:00 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 viewLoad = function() {
 	var viewsDoc = document.getElementById("views");
 	var hits = new XMLHttpRequest();
-	hits.open("GET", "https://api.countapi.xyz/hit/monupsr/monu.rf.gd");
+	hits.open("GET", "https://api.countapi.xyz/hit/monupsr");
 	hits.responseType = "json";
 	hits.onload = function() {
 		viewsDoc.innerHTML = `<sup><i>👁️ 0${this.response.value}</i></sup>`;
