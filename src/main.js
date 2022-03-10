280755e6ab4ccea2616b9dcf1eb200278a4c0f79
Monu Sharma <monupsr@yahoo.com>
2022-03-10 13:07:14 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 viewLoad = function() {
 	var viewsDoc = document.getElementById("views");
 	var hits = new XMLHttpRequest();
-	hits.open("GET", "https://api.countapi.xyz/hit/monupsr");
+	hits.open("GET", "http://monu.6te.net/tools/hit");
 	hits.responseType = "json";
 	hits.onload = function() {
 		viewsDoc.innerHTML = `<sup><i>👁️ 0${this.response.value}</i></sup>`;
